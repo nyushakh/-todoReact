69e4a9e8e882865e0a0d85e1303d54f145bb554d
nyushakh <RbwxNMpdbUiR7hm>
2020-05-09 20:14:16 +0300

    fix changed

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import styles from './Item.module.css';
 import clasnames from 'classnames';
 
@@ -12,12 +11,4 @@ const Item = ({ value, isDone }) => (
     }>{value}</span>
 );
 
-=======
-
-const Item = ({ value, isDone }) => (
-<span className={styles.item}> { value }</span>
-);
-
-
->>>>>>> e0501053a34aad2cb7463dde092cc700dee1c557
 export default Item;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Item from "../Item/Item";
-<<<<<<< HEAD
 import Checkbox from '@material-ui/core/Checkbox';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
@@ -26,12 +25,5 @@ const ItemList = ({items}) => (
         ))}
     </List>
 );
-=======
-
-const ItemList = ({ items }) => (<ul>
-    { items.map(item =>
-        <li key={ item.value }><Item value={item.value} isDone={item.isDone}/></li>)}
-</ul>);
->>>>>>> e0501053a34aad2cb7463dde092cc700dee1c557
 
 export default ItemList;
